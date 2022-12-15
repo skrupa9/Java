d1732c390f84a4359d32262bb4c3c992ec512d26
Szymon Krupa <skrupa@unitygroup.com>
2022-12-15 15:36:14 +0100

    Add MainApp

---
+++ src/MainApp.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(10);
+        System.out.println(10.5);
+        System.out.println("Text");
+        System.out.println('a');
+        System.out.println(true);
+        System.out.println(false);
+    }
+}
