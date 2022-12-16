fa50eba592d7491b73b8449f8a758db1cd2fbb4a
Szymon Krupa <skrupa@unitygroup.com>
2022-12-16 23:31:33 +0100

    Added Bug Class

--- src/MainApp.java
+++ src/MainApp.java
@@ -3,10 +3,8 @@ public class MainApp {
 
     public static void main(String[] args) {
 
-        User bartek = new User("Bartek", "Testowy", "test@test.pl", 25);
-        User tomek = new User("Tomek", "Brown", "tomek@brown.pl", 17);
 
-        bartek.getAllInfo();
-        tomek.getAllInfo();
+        Bug firstBug = new Bug("Connection Error", "test@test.com", 5);
+        firstBug.showAllInfo();
     }
 }
